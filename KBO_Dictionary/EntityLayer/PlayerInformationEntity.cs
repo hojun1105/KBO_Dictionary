905a26c008cc7b15f41d6f799f2bbc558a7e6fb0
hojunJeong <hojun1105@gmail.com>
2024-02-02 01:00:10 +0900

    entity만들기

---
+++ KBO_Dictionary/EntityLayer/PlayerInformationEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KBO_Dictionary.EntityLayer
+{
+    public class PlayerInformationEntity
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public int Team { get; set; }
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public DateTime Birth_date { get; set; }
+    }
+}
