e3d66a4524bceb68284b5683d9ed1e4e5e4c59f2
hojunJeong <hojun1105@gmail.com>
2024-02-14 01:00:12 +0900

    필요한 datamodel을 위한 인터페이스

---
+++ KBO_Dictionary/BusinessLogicLayer/IPlayerStatRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Windows.Documents;
+using KBO_Dictionary.DataAccessLayer;
+
+namespace KBO_Dictionary.BusinessLogicLayer;
+
+public interface IPlayerStatRepository
+{
+    public List<PlayerModel> SelectAllPlayerModels();
+}
