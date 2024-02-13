9d127a43deb58084065795106c1990f3315567f8
hojunJeong <hojun1105@gmail.com>
2024-02-14 00:53:19 +0900

    dbContext 생성

---
+++ KBO_Dictionary/DataAccessLayer/KboDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KBO_Dictionary.DataAccessLayer;
+
+public class KboDbContext : DbContext
+{
+    private const string Host = "localhost";
+    private const string User = "postgres";
+    private const string DbName = "postgres";
+    private const string Password = "991105";
+    private const string Port = "5432";
+    public DbSet<PlayerInformationEntity> PlayerInformation { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql($"Host={Host}:{Port};Database={DbName};Username={User};Password={Password}");
+    }
+}
