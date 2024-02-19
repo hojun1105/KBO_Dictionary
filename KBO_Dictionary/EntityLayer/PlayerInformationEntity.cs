de576028f673310d04fb037c73960d98f614bad1
hojunJeong <hojun1105@gmail.com>
2024-02-20 01:09:53 +0900

    entity 수정

--- KBO_Dictionary/EntityLayer/PlayerInformationEntity.cs
+++ KBO_Dictionary/EntityLayer/PlayerInformationEntity.cs
@@ -11,14 +11,29 @@ namespace KBO_Dictionary.EntityLayer
     [Table("PLAYER_INFO")]
     public class PlayerInformationEntity
     {
-        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity), Column("id")]
         public int Id { get; set; }
 
-        public string? Name { get; set; }
+        [Required]
+        [Column("name")]
+        public string Name { get; set; }
 
-        [ForeignKey("team_id")] public short TeamId { get; set; }
+        [Required]
+        [Column("height")]
         public double Height { get; set; }
-        public double Width { get; set; }
+
+        [Required]
+        [Column("weight")]
+        public double Weight { get; set; }
+
+        [Column("birth_date")]
         public DateTime BirthDate { get; set; }
+
+
+        [Column("team_id")]
+        public short TeamId { get; set; }
+
+        [ForeignKey("TeamId")]
+        public virtual TeamInformationEntity? TeamInformation { get; set; }
     }
 }
--- KBO_Dictionary/EntityLayer/TeamInformationEntity.cs
+++ KBO_Dictionary/EntityLayer/TeamInformationEntity.cs
@@ -1,9 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KBO_Dictionary.EntityLayer
 {
+    [Table("TEAM_INFO")]
+    public class TeamInformationEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        public string? Name { get; set; }
+
+        public ICollection<PlayerInformationEntity> Players { get; set; }
+    }
 }
